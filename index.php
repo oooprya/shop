8e0b615f1470a3f20975148d1e4a6bdbb4388a0e
oooprya <32761598+oooprya@users.noreply.github.com>
2017-11-15 17:18:25 +0200

    v4

--- index.php
+++ index.php
@@ -1,14 +1,3 @@
 <?php  ?>
 <h1>Hello</h1>
 <p>222</p>
-
-{
-	"accounts":
-	{
-		"GitHub":
-		{
-			"base_uri": "https://api.github.com",
-			"github_token": "1b5e75db019e3b23e9df94958a84fa66c290164f"
-		}
-	}
-}
